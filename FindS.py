532fe292a8cef66291b39a14d5d954338a690a75
Priyanshusinhaa <76002890+Priyanshusinhaa@users.noreply.github.com>
2022-03-05 14:54:07 +0530

    Add files via upload

---
+++ FindS.py
@@ -0,0 +1,29 @@
+
+students_skill = [['Writing','Reading','Painting','Programming','Googling', 'yes'], 
+                   ['Writing','Reading','Humoristic','Programming', 'Googling', 'yes'],
+                   ['Writing','Reading','Humoristic','Analytical Skill', 'googling', 'no'],
+                   ['Writing','Reading','Optimistic','Programming', 'Yahooing', 'yes'],
+                   ['Writing','Reading','Boxing','Reasoning','Binging', 'yes']]
+
+class FindS:
+    def __init__(self, hypothesis_ds):
+        #hypothesis_ds is a Hypothesis dataset
+        self.h0 = hypothesis_ds[0]
+        self.h_ds = hypothesis_ds
+
+    def mostSpecific(self):
+        for n in range(len(self.h_ds)):
+            if self.h_ds[n][-1] == 'yes' or 'Yes' or 'YES':
+                for m in range(len(self.h_ds[n])):
+                    if self.h0[m] != self.h_ds[n][m]:
+                        self.h0[m] = '?'
+        return self.h0
+
+specific = FindS(students_skill).mostSpecific()
+print(specific)
+
+
+
+
+
+
